c0b3b296e430efcf3b21bdd88dbf0b4a951f38da
Adrian Li <li.adrianmc@gmail.com>
2017-11-23 01:54:36 -0800

    clean up dependencies and fix the fs bug

---
+++ next.config.js
@@ -0,0 +1,20 @@
+// This file is not going through babel transformation.
+// So, we write it in vanilla JS
+// (But you could use ES2015 features supported by your Node.js version)
+
+module.exports = {
+  webpack: (config, { buildId, dev }) => {
+    // Perform customizations to webpack config
+
+    // Important: return the modified config
+    const newConfig = Object.assign({}, config, {
+      node: { fs: `empty` },
+    });
+    return newConfig;
+  },
+  webpackDevMiddleware: config =>
+    // Perform customizations to webpack dev middleware config
+
+    // Important: return the modified config
+    config,
+};
